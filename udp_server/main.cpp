ef4203f83dea8647154022232eca2ebdf4204eaf
Castor-Fiber <78352252+Castor-Fiber@users.noreply.github.com>
2023-02-27 12:41:32 +0300

    Add files via upload

---
+++ udp_server/main.cpp
@@ -0,0 +1,27 @@
+#include "udp_server.h"
+
+
+
+
+int main(int argc, char const *argv[])
+{
+
+    if (argc != 2)
+    {
+        std::cout << "Usage: " << argv[0] << " <port>" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    udp_server server(std::stoi(argv[1]));
+
+    if(!server.start())
+    {
+        std::cerr << server.get_last_error() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    server.run();
+
+    return EXIT_SUCCESS;
+}
+
